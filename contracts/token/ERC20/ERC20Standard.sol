ad299da64abc2095b66b9a056e776f8d7b2b17be
TakuyaObata <tky.obata@gmail.com>
2018-07-23 18:32:32 +0900

    Fixed the problem of approve() double-spend attack

--- contracts/token/ERC20/ERC20Standard.sol
+++ contracts/token/ERC20/ERC20Standard.sol
@@ -62,10 +62,14 @@ contract ERC20Standard is ERC20Interface {
    * and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
    * race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
    * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+   * 
+   * To avoid this issue, allowances are only allowed to be changed between zero and non-zero.
+   *
    * @param _spender The address which will spend the funds.
    * @param _value The amount of tokens to be spent.
    */
   function approve(address _spender, uint256 _value) external returns (bool) {
+    require(allowed[msg.sender][_spender] == 0 || _value == 0);
     allowed[msg.sender][_spender] = _value;
     emit Approval(msg.sender, _spender, _value);
     return true;
