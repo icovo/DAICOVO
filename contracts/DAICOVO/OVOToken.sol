aa720c672eb91789431f252a3810c6f6c82cbcca
TakuyaObata <tky.obata@gmail.com>
2018-08-10 20:59:23 +0900

    Got rid of old comments.

--- contracts/DAICOVO/OVOToken.sol
+++ contracts/DAICOVO/OVOToken.sol
@@ -5,8 +5,6 @@ import "./DaicovoStandardToken.sol";
 /**
  * @title OVO Token
  * @dev ERC20, ERC223 compliant mintable token.
- * @dev Extended with icon field to indicate IPFS hash for the token icon image.
- * @dev icon field compatible wallet app can load a token icon image from IPFS.
  */
 contract OVOToken is DaicovoStandardToken {
     constructor () public DaicovoStandardToken("ICOVO", "OVO", 9) {
